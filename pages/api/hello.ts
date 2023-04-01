a81e12a57d2620abec44e571fd3503162b82a42f
20yuteo <20yuteo@gmail.com>
2023-04-01 18:54:58 +0900

    notion clientを追加

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,5 +1,12 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import type { NextApiRequest, NextApiResponse } from 'next'
+import type { NextApiRequest, NextApiResponse } from 'next';
+import { Client } from "@notionhq/client";
+
+const notion = new Client({
+  auth: process.env.NOTION_TOKEN,
+})
+
+console.log(notion);
 
 type Data = {
   name: string
@@ -9,5 +16,5 @@ export default function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
-  res.status(200).json({ name: 'John Doe' })
+  res.status(200).json({ name: 'John' })
 }
