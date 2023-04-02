011758a2796801e5866e9645469ad27664769198
20yuteo <20yuteo@gmail.com>
2023-04-03 00:42:11 +0900

    postsを追加

---
+++ pages/api/posts.ts
@@ -0,0 +1,16 @@
+import type { NextApiRequest, NextApiResponse } from 'next'
+import { dbClient } from '../../config/db'
+import { PostRepository } from '../../repositories/postRepository';
+
+const postRepository = new PostRepository(dbClient);
+
+const handler = async (
+    req: NextApiRequest,
+    res: NextApiResponse
+) => {
+    const result = await postRepository.findAll();
+
+    res.status(200).json({ name: JSON.stringify(result) });
+}
+
+export default handler;
