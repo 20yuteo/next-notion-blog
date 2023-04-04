5741a83da728ed28d488d6e6b7d307dec96bdee8
20yuteo <20yuteo@gmail.com>
2023-04-04 23:00:26 +0900

    next.configを設定修正

--- next.config.js
+++ next.config.js
@@ -1,14 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
-
-module.exports = nextConfig
-
-module.exports = {
   env: {
     NOTION_TOKEN: process.env.NOTION_TOKEN,
     NOTION_DATABASE_ID: process.env.NOTION_DATABASE_ID,
     END_POINT: process.env.END_POINT,
   },
-};
+}
+
+module.exports = nextConfig
