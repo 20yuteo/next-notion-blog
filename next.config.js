7d970fce18921af546f71d9385dec1706c0418b9
20yuteo <20yuteo@gmail.com>
2023-04-04 22:55:49 +0900

    環境変数を設定

--- next.config.js
+++ next.config.js
@@ -4,3 +4,11 @@ const nextConfig = {
 }
 
 module.exports = nextConfig
+
+module.exports = {
+  env: {
+    NOTION_TOKEN: process.env.NOTION_TOKEN,
+    NOTION_DATABASE_ID: process.env.NOTION_DATABASE_ID,
+    END_POINT: process.env.END_POINT,
+  },
+};
