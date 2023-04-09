4555e3204caa7574c64da070168d0013755417e0
20yuteo <20yuteo@gmail.com>
2023-04-09 12:37:40 +0900

    skeltonのレスポンシブ対応

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,9 +4,12 @@ import dayjs from 'dayjs';
 import { useEffect, useState } from 'react';
 import { endpoint } from '../config/endpoint';
 import { IPost } from '../domain/Post';
+import { useWindowSize } from '../hooks/useWindowSize';
 
 export default function Home() {
   const [posts, setPosts] = useState<IPost[]>([]);
+
+  const [width] = useWindowSize();
 
   useEffect(() => {
     (async () => {
@@ -26,30 +29,31 @@ export default function Home() {
       <VStack flexGrow={2} maxH={"100%"}>
         <Container maxW='container.md'>
           <Heading size='md' margin={10}>記事一覧</Heading>
-          {posts.length ?
-            posts.map((post, index) => {
-              return (
-                <Card maxW={1024} marginBottom={8} key={index}>
-                  <CardBody>
-                    <Box>
-                      <Link href={`post/${post.id}?title=${post.title}`}>
-                        <Heading size='xs' textTransform='uppercase'>
-                          {post.title}
-                        </Heading>
-                        <Text pt='2' fontSize='sm'>
-                          投稿日: {post.createdAt.toString()}
-                        </Text>
-                      </Link>
-                    </Box>
-                  </CardBody>
-                </Card>
-              );
-            }) :
-            Array.from({ length: 6 }, (_, index) => (
-              <Stack key={index}>
-                <Skeleton height='20px' padding={10} borderRadius={8} marginBottom={8} />
-              </Stack>
-            ))
+          {
+            posts.length ?
+              posts.map((post, index) => {
+                return (
+                  <Card maxW={1024} marginBottom={8} key={index}>
+                    <CardBody>
+                      <Box>
+                        <Link href={`post/${post.id}?title=${post.title}`}>
+                          <Heading size='xs' textTransform='uppercase'>
+                            {post.title}
+                          </Heading>
+                          <Text pt='2' fontSize='sm'>
+                            投稿日: {post.createdAt.toString()}
+                          </Text>
+                        </Link>
+                      </Box>
+                    </CardBody>
+                  </Card>
+                );
+              }) :
+              Array.from({ length: 768 < width ? 6 : 4 }, (_, index) => (
+                <Stack key={index}>
+                  <Skeleton height='20px' size={"md"} padding={10} borderRadius={8} marginBottom={8} />
+                </Stack>
+              ))
           }
         </Container>
       </VStack>
